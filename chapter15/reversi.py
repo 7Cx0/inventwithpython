4fc523918f55f049064355fe13380f523ae38653
LG <lg2669@gmail.com>
2016-07-23 08:36:33 -0700

    save

--- chapter15/reversi.py
+++ chapter15/reversi.py
@@ -5,8 +5,8 @@ import sys
 
 def drawBoard(board):
     # This function prints out the board that it was passed. Returns None.
-    HLINE = ' +---+---+---+---+---+---+---+---+'
-    VLINE = ' |   |   |   |   |   |   |   |   |'
+    HLINE = '  +---+---+---+---+---+---+---+---+'
+    VLINE = '  |   |   |   |   |   |   |   |   |'
 
     print(' 1   2   3   4   5   6   7   8')
     print(HLINE)
